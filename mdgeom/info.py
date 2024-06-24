27558ccde95c8d8fd70ca43cd2dafa906bd25b4e
Sthitadhi Maiti <smaiti7@users.noreply.github.com>
2024-06-24 15:59:50 -0700

    Added info.py to code.

---
+++ mdgeom/info.py
@@ -0,0 +1,17 @@
+"""
+Information about Simulations
+==============================
+
+
+This module contains functions that provide overview info 
+about a trajectory
+
+"""
+
+
+def summary(*universe):
+    pass
+
+def extract(universe):
+    pass
+
